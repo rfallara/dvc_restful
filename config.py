1c4b68a426f0a6472950de668472a97c60bd586b
Ron Fallara <rofallar@cisco.com>
2018-10-01 11:23:56 -0400

    moved auth to standalone file

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import gcp_auth
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
@@ -6,6 +7,10 @@ PORT = 5000
 HOST = "127.0.0.1"
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}".format(DB_USER="***REMOVED***", DB_PASS="***REMOVED***", DB_ADDR="***REMOVED***", DB_NAME="dvc")
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}".format(
+                            DB_USER=gcp_auth.gcp_db['username'],
+                            DB_PASS=gcp_auth.gcp_db['password'],
+                            DB_ADDR=gcp_auth.gcp_db['db_addr'],
+                            DB_NAME=gcp_auth.gcp_db['db_name'])
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
