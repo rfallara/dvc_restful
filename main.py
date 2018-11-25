ba098848d707a9ba41e68038546d8bc212318760
Ron Fallara <rfallara@fallara.net>
2018-11-25 10:47:06 -0500

    Prepare for deployment to GAE

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ app = create_app('config')
 CORS(app)
 app.config['JWT_SECRET_KEY'] = gcp_auth.token_secret
 jwt = JWTManager(app)
+app.config['DEBUG'] = False
+app.run()
 
-print(__name__)
-if __name__ == '__main__':
-    app.run(host=app.config['HOST'],
-            port=app.config['PORT'],
-            debug=app.config['DEBUG'])
-
-if __name__ == 'builtins':
-    app.app_context().push()
+# print(__name__)
+# if __name__ == '__main__':
+#     app.run(host=app.config['HOST'],
+#             port=app.config['PORT'],
+#             debug=app.config['DEBUG'])
+#
+# if __name__ == 'builtins':
+#     app.app_context().push()
