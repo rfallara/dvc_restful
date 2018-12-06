ccf6f7f31055967df33c0aa9d21635e4748c4519
Ron Fallara <rofallar@cisco.com>
2018-12-05 20:51:55 -0500

    completed trip post request

--- views_trips.py
+++ views_trips.py
@@ -1,19 +1,21 @@
-from flask import request
+from flask import request, jsonify
+from sqlalchemy.exc import SQLAlchemyError
 from flask_restful import Resource
 from models import Trip, TripSchema, Owner, BookableRoom
 import status
 from flask_jwt_extended import jwt_required
 from dateutil.relativedelta import *
 from datetime import datetime
-from models import db
+from models import db, PersonalPoint, ActualPoint
+from sqlalchemy import or_
 
 trip_schema = TripSchema()
 
 
 class TripResource(Resource):
     @jwt_required
-    def get(self, id):
-        trip = Trip.query.get_or_404(id)
+    def get(self, trip_id):
+        trip = Trip.query.get_or_404(trip_id)
         result = trip_schema.dump(trip).data
         return result
 
@@ -36,13 +38,12 @@ class TripListResource(Resource):
             return errors, status.HTTP_400_BAD_REQUEST
         else:
             # create a new trip object and validate data
-            new_trip = Trip()
             trip_owner = Owner.query.filter_by(name=request_dict['owner']['name']).first()
             if trip_owner is None:
                 # owner does not exist
                 resp = {'message': 'owner does not exist'}
                 return resp, status.HTTP_400_BAD_REQUEST
-            new_trip.owner_id = trip_owner.id
+            # new_trip.owner_id = trip_owner.id
             trip_bookable_room = BookableRoom.query. \
                 filter(BookableRoom.resort.has(name=request_dict['bookable_room']['resort']['name'])). \
                 filter(BookableRoom.room_type.has(name=request_dict['bookable_room']['room_type']['name'])).first()
@@ -50,169 +51,165 @@ class TripListResource(Resource):
                 # Resort does not exist
                 resp = {'message': 'bookable room does not exist'}
                 return resp, status.HTTP_400_BAD_REQUEST
-            new_trip.bookable_room_id = trip_bookable_room.id
-            new_trip.check_in_date = request_dict['check_in_date']
-            new_trip.check_out_date = request_dict['check_out_date']
+            # new_trip.bookable_room_id = trip_bookable_room.id
+            new_trip = Trip(trip_owner, trip_bookable_room)
+            # new_trip.check_in_date = request_dict['check_in_date']
+            new_trip.check_in_date = datetime.strptime(request_dict['check_in_date'].split('T')[0], '%Y-%m-%d')
+            # new_trip.check_out_date = request_dict['check_out_date']
+            new_trip.check_out_date = datetime.strptime(request_dict['check_out_date'].split('T')[0], '%Y-%m-%d')
             if new_trip.check_in_date >= new_trip.check_out_date:
                 resp = {'message': 'check out must be after check in date'}
                 return resp, status.HTTP_400_BAD_REQUEST
-            new_trip.booked_date = request_dict['booked_date']
+            new_trip.booked_date = datetime.strptime(request_dict['booked_date'].split('T')[0], '%Y-%m-%d')
+            # new_trip.booked_date = request_dict['booked_date']
             new_trip.notes = request_dict['notes']
             new_trip.points_needed = request_dict['points_needed']
             if new_trip.points_needed < 0:
                 resp = {'message': 'points needed must be at least 1'}
                 return resp, status.HTTP_400_BAD_REQUEST
-        # New trip is valid now check for available points
-        current_use_year = datetime.strptime(new_trip.check_in_date.split('T')[0], '%Y-%m-%d')
+        db.session.flush()
+
+        # New trip is valid now try to allocate points
+
+        current_use_year = new_trip.check_in_date
         previous_use_year = current_use_year+relativedelta(years=-1)
         next_use_year = current_use_year+relativedelta(years=1)
         previous_two_use_year = current_use_year+relativedelta(years=-2)
-        booked_date = datetime.strptime(new_trip.booked_date.split('T')[0], '%Y-%m-%d')
+        booked_date = new_trip.booked_date
 
-        located_personal_point_ids = []
         personal_points_needed = new_trip.points_needed
-        count_banked_personal_points = 0
-        count_current_personal_points = 0
-        count_borrow_personal_points = 0
-
+        trip_owner_id = new_trip.owner_id
         # Find banked personal points
+        personal_points_banked = db.session.query(PersonalPoint)\
+            .filter(PersonalPoint.owner_id == trip_owner_id,
+                    PersonalPoint.trip_id.is_(None),
+                    PersonalPoint.use_year < previous_use_year)\
+            .order_by("use_year").order_by("point_number")\
+            .limit(personal_points_needed).all()
 
-        select_qry = "SELECT trip_id, id, use_year, point_number, owner_id"\
-                     " FROM personal_point"\
-                     " WHERE ((use_year) < '%s' AND ((owner_id)='%s') AND ((trip_id) Is Null))"\
-                     " ORDER BY use_year, point_number"\
-                     " LIMIT %s" % (previous_use_year.strftime('%Y-%m-%d'),
-                                    new_trip.owner_id,
-                                    personal_points_needed)
+        for point in personal_points_banked:
+            point.trip_id = new_trip.id
 
-        result = db.engine.execute(select_qry)
-        for row in result:
-            located_personal_point_ids.append(row['id'])
-            count_banked_personal_points += 1
-
-        personal_points_needed -= count_banked_personal_points
+        personal_points_needed -= len(personal_points_banked)
 
         # IF more points needed look for current personal points
         if personal_points_needed > 0:
 
-            select_qry = "SELECT trip_id, id, use_year, point_number, owner_id"\
-                         " FROM personal_point"\
-                         " WHERE ((use_year) < '%s' And (use_year) > '%s')"\
-                         " AND ((personal_point.owner_id)= '%s') AND ((trip_id) Is Null)"\
-                         " ORDER BY use_year, point_number"\
-                         " LIMIT %s" % (current_use_year,
-                                        previous_use_year,
-                                        new_trip.owner_id,
-                                        personal_points_needed)
-            result = db.engine.execute(select_qry)
-            for row in result:
-                located_personal_point_ids.append(row['id'])
-                count_current_personal_points += 1
+            personal_points_current = db.session.query(PersonalPoint)\
+                .filter(PersonalPoint.use_year < current_use_year, PersonalPoint.use_year > previous_use_year)\
+                .filter(PersonalPoint.owner_id == new_trip.owner_id)\
+                .filter(PersonalPoint.trip_id.is_(None))\
+                .order_by("use_year").order_by("point_number")\
+                .limit(personal_points_needed).all()
 
-            personal_points_needed -= count_current_personal_points
+            for point in personal_points_current:
+                point.trip_id = new_trip.id
+
+            personal_points_needed -= len(personal_points_current)
+        else:
+            personal_points_current = []
 
         # IF more points needed look for borrow personal points
         if personal_points_needed > 0:
 
-            select_qry = "SELECT trip_id, id, use_year, point_number, owner_id"\
-                         " FROM personal_point "\
-                         " WHERE (((use_year) < '%s' And (use_year) > '%s') AND ((owner_id) = '%s') AND ((trip_id) Is Null) ) "\
-                         " ORDER BY use_year, point_number "\
-                         " LIMIT %s" % (next_use_year,
-                                        current_use_year,
-                                        new_trip.owner_id,
-                                        personal_points_needed)
+            personal_points_borrow = db.session.query(PersonalPoint) \
+                .filter(PersonalPoint.use_year < next_use_year, PersonalPoint.use_year > current_use_year) \
+                .filter(PersonalPoint.owner_id == new_trip.owner_id) \
+                .filter(PersonalPoint.trip_id.is_(None)) \
+                .order_by("use_year").order_by("point_number") \
+                .limit(personal_points_needed).all()
 
-            result = db.engine.execute(select_qry)
-            for row in result:
-                located_personal_point_ids.append(row['id'])
-                count_borrow_personal_points += 1
+            for point in personal_points_borrow:
+                point.trip_id = new_trip.id
 
-            personal_points_needed -= count_borrow_personal_points
+            personal_points_needed -= len(personal_points_borrow)
+        else:
+            personal_points_borrow = []
 
         # IF more personal points requirement was satisfied
         if personal_points_needed > 0:
             print('Points needed - ' + str(new_trip.points_needed))
-            print('Personal Banked Points - ' + str(count_banked_personal_points))
-            print('Personal Current Points - ' + str(count_current_personal_points))
-            print('Personal Borrow Points - ' + str(count_borrow_personal_points))
+            print('Personal Banked Points - ' + str(len(personal_points_banked)))
+            print('Personal Current Points - ' + str(len(personal_points_current)))
+            print('Personal Borrow Points - ' + str(len(personal_points_borrow)))
+            new_trip.delete(new_trip, "Removing Trip - Personal Points Short")
             resp = {'message': 'personal points shortage of %s points' % personal_points_needed}
             return resp, status.HTTP_400_BAD_REQUEST
 
         # FIND actual points
-        located_actual_point_ids = []
         actual_points_needed = new_trip.points_needed
-        count_banked_actual_points = 0
-        count_current_actual_points = 0
-        count_borrow_actual_points = 0
 
-        select_qry = "SELECT trip_id, id, use_year, point_number "\
-                     " FROM actual_point "\
-                     " WHERE (( use_year < '%s' And use_year > '%s') "\
-                     " AND (trip_id Is Null) "\
-                     " AND (banked_date < '%s') ) "\
-                     " ORDER BY use_year, point_number "\
-                     " LIMIT %s" % (previous_use_year,
-                                    previous_two_use_year,
-                                    booked_date,
-                                    actual_points_needed)
+        actual_points_banked = db.session.query(ActualPoint)\
+            .filter(ActualPoint.use_year < previous_use_year, ActualPoint.use_year > previous_two_use_year)\
+            .filter(ActualPoint.trip_id.is_(None))\
+            .filter(ActualPoint.banked_date < booked_date) \
+            .order_by("use_year").order_by("point_number") \
+            .limit(actual_points_needed).all()
 
-        result = db.engine.execute(select_qry)
-        for row in result:
-            located_actual_point_ids.append(row['id'])
-            count_banked_actual_points += 1
+        for point in actual_points_banked:
+            point.trip_id = new_trip.id
 
-        actual_points_needed -= count_banked_actual_points
+        actual_points_needed -= len(actual_points_banked)
 
         # IF actual points requirement is satisfied
         if actual_points_needed > 0:
-            select_qry = "SELECT trip_id, id, use_year, point_number"\
-                         " FROM actual_point"\
-                         " WHERE (((use_year) < '%s' And (use_year) > '%s') "\
-                         " AND ((trip_id) Is Null) AND (banked_date Is Null Or banked_date > '%s')  )"\
-                         " ORDER BY use_year, point_number"\
-                         " LIMIT %s" % (current_use_year,
-                                        previous_two_use_year,
-                                        booked_date,
-                                        actual_points_needed)
-            result = db.engine.execute(select_qry)
-            for row in result:
-                located_actual_point_ids.append(row['id'])
-                count_current_actual_points += 1
 
-            actual_points_needed -= count_current_actual_points
+            actual_points_current = db.session.query(ActualPoint)\
+                .filter(ActualPoint.use_year < current_use_year, ActualPoint.use_year > previous_two_use_year)\
+                .filter(ActualPoint.trip_id.is_(None))\
+                .filter(or_(ActualPoint.banked_date.is_(None), ActualPoint.banked_date > booked_date)) \
+                .order_by("use_year").order_by("point_number") \
+                .limit(actual_points_needed).all()
+
+            for point in actual_points_current:
+                point.trip_id = new_trip.id
+
+            actual_points_needed -= len(actual_points_current)
+        else:
+            actual_points_current = []
 
         # IF actual points requirement is satisfied
         if actual_points_needed > 0:
-            select_qry = "SELECT trip_id, id, use_year, point_number"\
-                         " FROM actual_point"\
-                         " WHERE (((use_year) < '%s' And (use_year) > '%s') "\
-                         " AND ((trip_id) Is Null) )"\
-                         " ORDER BY use_year, point_number"\
-                         " LIMIT %s" % (next_use_year,
-                                        current_use_year,
-                                        actual_points_needed)
-            result = db.engine.execute(select_qry)
-            for row in result:
-                located_actual_point_ids.append(row['id'])
-                count_borrow_actual_points += 1
 
-            actual_points_needed -= count_borrow_actual_points
+            actual_points_borrow = db.session.query(ActualPoint)\
+                .filter(ActualPoint.use_year < next_use_year, ActualPoint.use_year > current_use_year)\
+                .filter(ActualPoint.trip_id.is_(None))\
+                .order_by("use_year").order_by("point_number")\
+                .limit(actual_points_needed).all()
+
+            for point in actual_points_borrow:
+                point.trip_id = new_trip.id
+
+            actual_points_needed -= len(actual_points_borrow)
+        else:
+            actual_points_borrow = []
 
         # IF more personal points requirement was satisfied
         if actual_points_needed > 0:
             print('Points needed - ' + str(new_trip.points_needed))
-            print('Actual Banked Points - ' + str(count_banked_actual_points))
-            print('Actual Current Points - ' + str(count_current_actual_points))
-            print('Actual Borrow Points - ' + str(count_borrow_actual_points))
+            print('Actual Banked Points - ' + str(len(actual_points_banked)))
+            print('Actual Current Points - ' + str(len(actual_points_current)))
+            print('Actual Borrow Points - ' + str(len(actual_points_borrow)))
+            new_trip.delete(new_trip, "Removing Trip - Actual Point Short")
             resp = {'message': 'actual points shortage of %s points' % actual_points_needed}
             return resp, status.HTTP_400_BAD_REQUEST
 
         print('All points requirements satisfied, ok to book trip')
         print('Points needed - ' + str(new_trip.points_needed))
-        print('Personal Banked Points - ' + str(count_banked_personal_points))
-        print('Personal Current Points - ' + str(count_current_personal_points))
-        print('Personal Borrow Points - ' + str(count_borrow_personal_points))
-        print('Actual Banked Points - ' + str(count_banked_actual_points))
-        print('Actual Current Points - ' + str(count_current_actual_points))
-        print('Actual Borrow Points - ' + str(count_borrow_actual_points))
+        print('Personal Banked Points - ' + str(len(personal_points_banked)))
+        print('Personal Current Points - ' + str(len(personal_points_current)))
+        print('Personal Borrow Points - ' + str(len(personal_points_borrow)))
+        print('Actual Banked Points - ' + str(len(actual_points_banked)))
+        print('Actual Current Points - ' + str(len(actual_points_current)))
+        print('Actual Borrow Points - ' + str(len(actual_points_borrow)))
+
+        # ALL is good commit to db and return success
+        try:
+            db.session.commit()
+            result = trip_schema.dump(new_trip).data
+            return result, status.HTTP_201_CREATED
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            resp = jsonify({"error": str(e)})
+            resp.status_code = status.HTTP_400_BAD_REQUEST
+            return resp
