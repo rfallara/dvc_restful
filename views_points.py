98f1fcd6d51c1eb692292d95efc53578f834eace
Ron Fallara <rfallara@fallara.net>
2019-01-29 17:20:46 -0500

    added points count resource

--- views_points.py
+++ views_points.py
@@ -24,40 +24,44 @@ class ActualPointListResource(Resource):
         return result
 
 
-class ActualPointCountResource(Resource):
-    @jwt_required
-    def get(self):
-        reference_date = datetime.datetime.now()
-
-        banked_count = len(ActualPoint.query.
+def get_actual_point_count(reference_date):
+    banked_count = len(ActualPoint.query.
                            filter(ActualPoint.use_year < (reference_date + relativedelta(years=-1)),
                                   ActualPoint.use_year > (reference_date + relativedelta(years=-2))).
                            filter(ActualPoint.trip_id.is_(None)).
                            filter(ActualPoint.banked_date.isnot(None)).
                            all())
-        current_count = len(ActualPoint.query.
-                            filter(ActualPoint.use_year < reference_date,
-                                   ActualPoint.use_year > (reference_date + relativedelta(years=-1))).
-                            filter(ActualPoint.trip_id.is_(None)).
-                            filter(ActualPoint.banked_date.is_(None)).
-                            all())
-        current_banked_count = len(ActualPoint.query.
-                                   filter(ActualPoint.use_year < reference_date,
-                                          ActualPoint.use_year > (reference_date + relativedelta(years=-1))).
-                                   filter(ActualPoint.trip_id.is_(None)).
-                                   filter(ActualPoint.banked_date.isnot(None)).
-                                   all())
-        borrow_count = len(ActualPoint.query.
-                           filter(ActualPoint.use_year < (reference_date + relativedelta(years=+1)),
-                                  ActualPoint.use_year > reference_date).
-                           filter(ActualPoint.trip_id.is_(None)).
-                           filter(ActualPoint.banked_date.is_(None)).
-                           all())
+    current_count = len(ActualPoint.query.
+                        filter(ActualPoint.use_year < reference_date,
+                               ActualPoint.use_year > (reference_date + relativedelta(years=-1))).
+                        filter(ActualPoint.trip_id.is_(None)).
+                        filter(ActualPoint.banked_date.is_(None)).
+                        all())
+    current_banked_count = len(ActualPoint.query.
+                               filter(ActualPoint.use_year < reference_date,
+                                      ActualPoint.use_year > (reference_date + relativedelta(years=-1))).
+                               filter(ActualPoint.trip_id.is_(None)).
+                               filter(ActualPoint.banked_date.isnot(None)).
+                               all())
+    borrow_count = len(ActualPoint.query.
+                       filter(ActualPoint.use_year < (reference_date + relativedelta(years=+1)),
+                              ActualPoint.use_year > reference_date).
+                       filter(ActualPoint.trip_id.is_(None)).
+                       filter(ActualPoint.banked_date.is_(None)).
+                       all())
 
-        return {'banked': banked_count,
-                'current': current_count,
-                'current_banked': current_banked_count,
-                'borrow': borrow_count}
+    return {'banked': banked_count,
+            'current': current_count,
+            'current_banked': current_banked_count,
+            'borrow': borrow_count}
+
+
+class ActualPointCountResource(Resource):
+    @jwt_required
+    def get(self):
+        reference_date = datetime.datetime.now()
+        return get_actual_point_count(reference_date)
+
 
 
 class PersonalPointResource(Resource):
@@ -76,30 +80,39 @@ class PersonalPointListResource(Resource):
         return result
 
 
+def get_personal_point_count(owner_id, reference_date):
+    banked_count = len(PersonalPoint.query.
+                       filter(PersonalPoint.use_year < (reference_date + relativedelta(years=-1))).
+                       filter(PersonalPoint.trip_id.is_(None)).
+                       filter(PersonalPoint.owner_id == owner_id).all())
+    current_count = len(PersonalPoint.query.
+                        filter(PersonalPoint.use_year < reference_date,
+                               PersonalPoint.use_year > (reference_date + relativedelta(years=-1))).
+                        filter(PersonalPoint.trip_id.is_(None)).
+                        filter(PersonalPoint.owner_id == owner_id).all())
+    borrow_count = len(PersonalPoint.query.
+                       filter(PersonalPoint.use_year < (reference_date + relativedelta(years=+1)),
+                              PersonalPoint.use_year > reference_date).
+                       filter(PersonalPoint.trip_id.is_(None)).
+                       filter(PersonalPoint.owner_id == owner_id).all())
+    return {'banked': banked_count,
+            'current': current_count,
+            'borrow': borrow_count}
+
+
 class PersonalPointCountResource(Resource):
     @jwt_required
     def get(self, owner_id):
         reference_date = datetime.datetime.now()
-        # parser = reqparse.RequestParser()
-        # parser.add_argument('owner_id', help='ID of owner')
-        # args = parser.parse_args()
-        # owner_id = args['owner_id']
+        return get_personal_point_count(owner_id, reference_date)
 
-        banked_count = len(PersonalPoint.query.
-                           filter(PersonalPoint.use_year < (reference_date + relativedelta(years=-1))).
-                           filter(PersonalPoint.trip_id.is_(None)).
-                           filter(PersonalPoint.owner_id == owner_id).all())
-        current_count = len(PersonalPoint.query.
-                            filter(PersonalPoint.use_year < reference_date,
-                                   PersonalPoint.use_year > (reference_date + relativedelta(years=-1))).
-                            filter(PersonalPoint.trip_id.is_(None)).
-                            filter(PersonalPoint.owner_id == owner_id).all())
-        borrow_count = len(PersonalPoint.query.
-                           filter(PersonalPoint.use_year < (reference_date + relativedelta(years=+1)),
-                                  PersonalPoint.use_year > reference_date).
-                           filter(PersonalPoint.trip_id.is_(None)).
-                           filter(PersonalPoint.owner_id == owner_id).all())
-        return {'banked': banked_count,
-                'current': current_count,
-                'borrow': borrow_count}
 
+class PointCount(Resource):
+    @jwt_required
+    def get(self, owner_id):
+        reference_date = datetime.datetime.now()
+        personal_points = get_personal_point_count(owner_id, reference_date)
+        actual_points = get_actual_point_count(reference_date)
+
+        return {'actual_points': actual_points,
+                'personal_points': personal_points}
