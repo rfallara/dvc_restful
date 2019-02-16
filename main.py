f277fcb51f11e7e0e1a39fafa2d5a2c41838f7ce
Ron Fallara <rofallar@cisco.com>
2019-02-15 21:39:34 -0500

    Prepare for GCP deployment

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 from app import create_app
-from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 import gcp_auth
 
 app = create_app('config')
-CORS(app)
 app.config['JWT_SECRET_KEY'] = gcp_auth.token_secret
 jwt = JWTManager(app)
 
 
 if app.config['ENV'].upper() == 'DEVELOPMENT':
     print('DEVELOPMENT ENVIRONMENT')
-    # print(__name__)
-    # app.run()
 else:
     print('PRODUCTION ENVIRONMENT')
 
