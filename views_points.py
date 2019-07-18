d8bfa61175b219bedf69153e7b5a5d34642789c2
Ron Fallara <rofallar@cisco.com>
2019-07-18 13:11:09 -0400

    created bank points calls

--- views_points.py
+++ views_points.py
@@ -1,8 +1,11 @@
 from flask_restful import Resource, reqparse
-from models import ActualPoint, ActualPointScheme, PersonalPoint, PersonalPointSchema, Owner, OwnerEmail
+from flask import jsonify, request
+from models import db, ActualPoint, ActualPointScheme, PersonalPoint, PersonalPointSchema,\
+    Owner, OwnerEmail, EventLogger
 from flask_jwt_extended import jwt_required
 from dateutil.relativedelta import *
 import datetime
+import status
 
 actual_point_schema = ActualPointScheme()
 personal_point_schema = PersonalPointSchema()
@@ -121,3 +124,46 @@ class PointCount(Resource):
 
         return {'actual_points': actual_points,
                 'personal_points': personal_points}
+
+
+class BankPointCountResource(Resource):
+    @jwt_required
+    def get(self, epoch_bank_date):
+        bank_date = datetime.datetime.fromtimestamp(epoch_bank_date)
+        banked_count = len(ActualPoint.query.
+                           filter(ActualPoint.use_year < bank_date,
+                                  ActualPoint.use_year > (bank_date + relativedelta(years=-1))).
+                           filter(ActualPoint.trip_id.is_(None)).filter(ActualPoint.banked_date.is_(None)).
+                           all())
+        return {'available_bank_count': banked_count,
+                'bank_date': bank_date.strftime('%Y-%m-%d')}
+
+
+class BankPointResource(Resource):
+    @jwt_required
+    def post(self):
+        request_dict = request.get_json()
+        if not request_dict:
+            resp = {'message': 'No input data provided'}
+            return resp, status.HTTP_400_BAD_REQUEST
+        bank_date = datetime.datetime.fromtimestamp(request_dict['epoch_bank_date'])
+        bankable_points = ActualPoint.query.\
+            filter(ActualPoint.use_year < bank_date,
+                   ActualPoint.use_year > (bank_date + relativedelta(years=-1))).\
+            filter(ActualPoint.trip_id.is_(None)).filter(ActualPoint.banked_date.is_(None)).\
+            limit(request_dict['count_to_bank']).all()
+
+        if request_dict['count_to_bank'] > len(bankable_points):
+            resp = jsonify({"error": "requested bank is more than available bankable points"})
+            resp.status_code = status.HTTP_400_BAD_REQUEST
+            return resp
+
+        for point in bankable_points:
+            point.banked_date = bank_date
+
+        log = 'POINTS BANKED - %s points banked with a bank date of %s' % (len(bankable_points), bank_date.strftime('%Y-%m-%d %H:%M:%S'))
+
+        db.session.add(EventLogger('test@google.com', log))
+        db.session.commit()
+
+
